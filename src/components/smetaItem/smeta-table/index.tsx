403ea4a0e1bed8c09c3cbfb88d6267a6cfa91220
nuruddin <sg7720@gmail.com>
2024-11-08 15:21:30 +0300

    multirow in textfield

--- src/components/smetaItem/smeta-table/index.tsx
+++ src/components/smetaItem/smeta-table/index.tsx
@@ -1,8 +1,8 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import "./style.smetatable.scss"
-import {TextField} from "@mui/material";
-import {CommonButton as Button} from '../../../common/components/button'
-import {DatePicker, LocalizationProvider} from "@mui/lab";
+import { TextField } from "@mui/material";
+import { CommonButton as Button } from '../../../common/components/button'
+import { DatePicker, LocalizationProvider } from "@mui/lab";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import ruLocale from "date-fns/locale/ru";
 
@@ -29,139 +29,143 @@ const SmetaTable: FC<{
         handleChangeCosts,
     }) => {
 
-    const addDataToTable = () => {
-        addTableItem()
-    }
+        const addDataToTable = () => {
+            addTableItem()
+        }
 
-    const removeDataToTable = (id: number) => {
-        delTableItem(id)
-    }
+        const removeDataToTable = (id: number) => {
+            delTableItem(id)
+        }
 
-    const handleChangeRoadCosts = (id: number, keyVal: string, val: string) => {
-        handleChangeCosts(id, keyVal, val)
-    }
+        const handleChangeRoadCosts = (id: number, keyVal: string, val: string) => {
+            handleChangeCosts(id, keyVal, val)
+        }
 
 
-    return <div className='smeta-table'>
-        <div className={'diagnosis-table'}>
-            <div className={'table-section'}>
-                <table>
-                    <thead>
-                    <tr>
-                        {headers.map(el => <td>
-                            {el.hdr}
-                        </td>)}
-                        <td>
+        return <div className='smeta-table'>
+            <div className={'diagnosis-table'}>
+                <div className={'table-section'}>
+                    <table>
+                        <thead>
+                            <tr>
+                                {headers.map(el => <td>
+                                    {el.hdr}
+                                </td>)}
+                                <td>
 
-                        </td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {data.length > 0 && data.map((el: any, index: number) => (<tr>
-                        {headers.map(hdrEl => <td>
-                            {!hdrEl.isDate ? <TextField size={'small'}
-                                                        value={el[hdrEl.field]}
-                                                        onChange={(e) => {
-                                                            handleChangeRoadCosts(index, hdrEl.field, e.target.value)
-                                                        }}
-                                                        error={error}
-                                                        fullWidth
-                                                        sx={{
-                                                            "& fieldset": {border: 'none'},
-                                                        }}
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.length > 0 && data.map((el: any, index: number) => (<tr>
+                                {headers.map(hdrEl => <td>
+                                    {!hdrEl.isDate ? <TextField size={'small'}
+                                        value={el[hdrEl.field]}
+                                        onChange={(e) => {
+                                            handleChangeRoadCosts(index, hdrEl.field, e.target.value)
+                                        }}
+                                        error={error}
+                                        fullWidth
+                                        multiline
+                                        maxRows={5}
+                                        sx={{
+                                            "& fieldset": { border: 'none' },
+                                        }}
 
-                            /> : <td>
-                                <LocalizationProvider dateAdapter={AdapterDateFns} locale={ruLocale}>
-                                    <div>
-                                        <DatePicker
-                                            mask='__.__.____'
-                                            value={el[hdrEl.field]}
-                                            toolbarPlaceholder='19.06.87'
-                                            onChange={(e) => {
-                                                handleChangeRoadCosts(index, hdrEl.field, e)
-                                            }}
-                                            renderInput={(params: any) =>
-                                                <TextField
-                                                    {...params}
-                                                    size='small'
-                                                    error={error}
-                                                    placeholder={el.hdr}
-                                                    fullWidth
-                                                    sx={{
-                                                        "& fieldset": {border: 'none', padding: '0px'},
+                                    /> : <td>
+                                        <LocalizationProvider dateAdapter={AdapterDateFns} locale={ruLocale}>
+                                            <div>
+                                                <DatePicker
+                                                    mask='__.__.____'
+                                                    value={el[hdrEl.field]}
+                                                    toolbarPlaceholder='19.06.87'
+                                                    onChange={(e) => {
+                                                        handleChangeRoadCosts(index, hdrEl.field, e)
                                                     }}
-                                                />}
-                                            inputFormat="dd.MM.yyyy"
-                                        />
-                                    </div>
-                                </LocalizationProvider></td>}
-                        </td>)}
-                        <td>
-                            <Button onClick={() => removeDataToTable(index)} title={'-'} color={'error'}/>
-                        </td>
-                    </tr>))}
-                    <tr>
-                        {headers.map(el => {
-                            return el.isDate ? <td>
-                                    <LocalizationProvider dateAdapter={AdapterDateFns} locale={ruLocale}>
-                                        <div>
-                                            <DatePicker
-                                                mask='__.__.____'
+                                                    renderInput={(params: any) =>
+                                                        <TextField
+                                                            {...params}
+                                                            size='small'
+                                                            error={error}
+                                                            placeholder={el.hdr}
+                                                            fullWidth
+                                                            sx={{
+                                                                "& fieldset": { border: 'none', padding: '0px' },
+                                                            }}
+                                                        />}
+                                                    inputFormat="dd.MM.yyyy"
+                                                />
+                                            </div>
+                                        </LocalizationProvider></td>}
+                                </td>)}
+                                <td>
+                                    <Button onClick={() => removeDataToTable(index)} title={'-'} color={'error'} />
+                                </td>
+                            </tr>))}
+                            <tr>
+                                {headers.map(el => {
+                                    return el.isDate ? <td>
+                                        <LocalizationProvider dateAdapter={AdapterDateFns} locale={ruLocale}>
+                                            <div>
+                                                <DatePicker
+                                                    mask='__.__.____'
+                                                    value={localObj[el.field as any] || ''}
+                                                    toolbarPlaceholder='19.06.87'
+                                                    onChange={(e) => {
+                                                        if (error) {
+                                                            setError(false)
+                                                        }
+                                                        setLocalObj((state: any) => ({
+                                                            ...state,
+                                                            [el.field as any]: e
+                                                        }))
+                                                    }}
+                                                    renderInput={(params: any) =>
+                                                        <TextField
+                                                            {...params}
+                                                            size='small'
+                                                            error={error}
+                                                            placeholder={'20.20.2024'}
+                                                            fullWidth
+                                                        />}
+                                                    inputFormat="dd.MM.yyyy"
+                                                />
+                                            </div>
+                                        </LocalizationProvider></td>
+                                        : <td>
+                                            <TextField size={'small'}
+                                                required
                                                 value={localObj[el.field as any] || ''}
-                                                toolbarPlaceholder='19.06.87'
                                                 onChange={(e) => {
                                                     if (error) {
                                                         setError(false)
                                                     }
                                                     setLocalObj((state: any) => ({
                                                         ...state,
-                                                        [el.field as any]: e
+                                                        [el.field as any]: e.target.value
                                                     }))
                                                 }}
-                                                renderInput={(params: any) =>
-                                                    <TextField
-                                                        {...params}
-                                                        size='small'
-                                                        error={error}
-                                                        placeholder={'20.20.2024'}
-                                                        fullWidth
-                                                    />}
-                                                inputFormat="dd.MM.yyyy"
-                                            />
-                                        </div>
-                                    </LocalizationProvider></td>
-                                : <td>
-                                    <TextField size={'small'}
-                                               required
-                                               value={localObj[el.field as any] || ''}
-                                               onChange={(e) => {
-                                                   if (error) {
-                                                       setError(false)
-                                                   }
-                                                   setLocalObj((state: any) => ({
-                                                       ...state,
-                                                       [el.field as any]: e.target.value
-                                                   }))
-                                               }}
-                                               error={error}
-                                               placeholder={el.hdr}
-                                               fullWidth
+                                                error={error}
+                                                placeholder={el.hdr}
+                                                fullWidth
+                                                multiline
+                                                maxRows={5}
 
-                                    />
+                                            />
+                                        </td>
+                                })}
+                                <td className={'add-add-btn'}>
+                                    <Button onClick={addDataToTable} title={'+'} />
                                 </td>
-                        })}
-                        <td className={'add-add-btn'}>
-                            <Button onClick={addDataToTable} title={'+'}/>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div>
-                <table>
-                </table>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <table>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-}
+    }
 export default SmetaTable
