4194bfd14834a43da451dd15d7e991024759433a
nuruddin <sg7720@gmail.com>
2024-11-15 16:03:30 +0300

    fix не работала смена менеджера

--- src/components/reportlist/reportlist.tsx
+++ src/components/reportlist/reportlist.tsx
@@ -1,23 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {Typography, Pagination, CircularProgress} from "@mui/material";
-import React, {useEffect} from "react";
+import { Typography, Pagination, CircularProgress } from "@mui/material";
+import React, { useEffect } from "react";
 import './style.reportlist.scss'
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../app/store";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../app/store";
 import AddModal from "./add_modal";
-import {IconButton, Box} from '@mui/material';
+import { IconButton, Box } from '@mui/material';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import {useHistory} from "react-router-dom";
-import {openModal, setStatus, openManagerChangeModal} from "../../reducers/ui";
-import {isEmpty} from "lodash";
-import {selectApplicationUserRights} from "../../common/selectors/user";
+import { useHistory } from "react-router-dom";
+import { openModal, setStatus, openManagerChangeModal } from "../../reducers/ui";
+import { isEmpty } from "lodash";
+import { selectApplicationUserRights } from "../../common/selectors/user";
 import BlockIcon from '@mui/icons-material/Block';
 import EditIcon from '@mui/icons-material/Edit';
 import ManagerChangeModal from "./manager_change_modal";
-import {useFetchApplications} from "../../common/hooks/useFetchApplications";
+import { useFetchApplications } from "../../common/hooks/useFetchApplications";
 import TableHeader from "../../common/components/tableheader/tableHeader";
 import BasicModal from "../../common/components/modal/ConsiliumModal";
-import {CommonButton} from "../../common/components/button";
+import { CommonButton } from "../../common/components/button";
 
 const ReportList = (): React.ReactElement => {
     const dispatch = useDispatch()
@@ -26,7 +26,7 @@ const ReportList = (): React.ReactElement => {
         isModalOpened, status, errorMessage,
         isManagerChangeModalOpened
     } = useSelector((state: RootState) => state.ui)
-    const {appls, isLoading, fetchApp, error, id, role, deleteAppl} = useFetchApplications()
+    const { appls, isLoading, fetchApp, error, id, role, deleteAppl } = useFetchApplications()
     const rights = useSelector((state: RootState) => selectApplicationUserRights(state))
     const [page, setPage] = React.useState(1);
     const [patientName, setPatientName] = React.useState('');
@@ -49,14 +49,14 @@ const ReportList = (): React.ReactElement => {
         field: patientName,
         onChange: setPatientName
     }, 'Дата рождения', {
-        title: 'Запрос пациента',
-        field: patientRequest,
-        onChange: setPatientRequest
-    }, {title: 'Название фонда', field: fundName, onChange: setFundName}, {
-        title: 'Запрос фонда',
-        field: fundRequest,
-        onChange: setFundRequest
-    }, {title: 'Ответственный', field: manager, onChange: setManager}, 'Дата создания', 'Дата исполнения', 'Удалить']
+            title: 'Запрос пациента',
+            field: patientRequest,
+            onChange: setPatientRequest
+        }, { title: 'Название фонда', field: fundName, onChange: setFundName }, {
+            title: 'Запрос фонда',
+            field: fundRequest,
+            onChange: setFundRequest
+        }, { title: 'Ответственный', field: manager, onChange: setManager }, 'Дата создания', 'Дата исполнения', 'Удалить']
 
     useEffect(() => {
         if (id !== undefined && id !== '') {
@@ -84,54 +84,54 @@ const ReportList = (): React.ReactElement => {
         history.push(`application/${id}`)
     }
     return <div className='add-appl-container'>
-        {isModalOpened && <AddModal fetchApp={fetchApp}/>}
-        {isManagerChangeModalOpened && <ManagerChangeModal appls={appls}/>}
+        {isModalOpened && <AddModal fetchApp={fetchApp} />}
+        {isManagerChangeModalOpened && <ManagerChangeModal appls={appls.rows} />}
         <div className='add-button-wrapper'>
             {isManagerChangeModalOpened === undefined && rights.processedRights.applications?.create &&
                 <CommonButton
                     onClick={() => dispatch(openModal(true))}
-                    title={'Новое заключение'}/>}
+                    title={'Новое заключение'} />}
 
         </div>
         <div className="appl-table">
             <table>
                 <thead>
-                <TableHeader tableData={tableData} role={role}
-                             isDeleteRights={rights.processedRights.applications?.delete}
-                             isManagerChange={role !== 'doctor'}/>
+                    <TableHeader tableData={tableData} role={role}
+                        isDeleteRights={rights.processedRights.applications?.delete}
+                        isManagerChange={role !== 'doctor'} />
                 </thead>
                 <tbody>
-                {!isLoading && appls.rows.length > 0 && appls.rows.map((appl: any, index: number) => <tr
-                    onClick={() => goToApplItem(appl.id)} key={appl.patientName}>
-                    <td>{(page * 10 - 10) + 1 + index}</td>
-                    <td>{appl.patientName}</td>
-                    <td>{new Date(appl.patientBirthDate).toLocaleString()}</td>
-                    <td>{appl.patientRequest}</td>
-                    <td>{appl.fundName}</td>
-                    <td>{appl.fundRequest}</td>
-                    {role !== 'doctor' && <td>{appl.manager}</td>}
-                    <td>{new Date(appl.creationDate).toLocaleString()}</td>
-                    <td>{appl.execDate && new Date(appl.execDate).toLocaleString()}</td>
-                    {(rights.processedRights.applications?.delete) &&
-                        <td><IconButton className='delete-button' onClick={(e: any) => {
-                            e.stopPropagation()
-                            appl.id && setDeleteModal(appl.id)
-                        }}>
-                            <DeleteOutlineIcon/>
-                        </IconButton></td>}
-                    {role !== 'doctor' && <td><IconButton>
-                        <EditIcon onClick={(e: any) => {
-                            e.stopPropagation();
-                            dispatch(openManagerChangeModal(appl.id))
-                        }}/></IconButton>
-                    </td>}
-                </tr>)}
+                    {!isLoading && appls.rows.length > 0 && appls.rows.map((appl: any, index: number) => <tr
+                        onClick={() => goToApplItem(appl.id)} key={appl.patientName}>
+                        <td>{(page * 10 - 10) + 1 + index}</td>
+                        <td>{appl.patientName}</td>
+                        <td>{new Date(appl.patientBirthDate).toLocaleString()}</td>
+                        <td>{appl.patientRequest}</td>
+                        <td>{appl.fundName}</td>
+                        <td>{appl.fundRequest}</td>
+                        {role !== 'doctor' && <td>{appl.manager}</td>}
+                        <td>{new Date(appl.creationDate).toLocaleString()}</td>
+                        <td>{appl.execDate && new Date(appl.execDate).toLocaleString()}</td>
+                        {(rights.processedRights.applications?.delete) &&
+                            <td><IconButton className='delete-button' onClick={(e: any) => {
+                                e.stopPropagation()
+                                appl.id && setDeleteModal(appl.id)
+                            }}>
+                                <DeleteOutlineIcon />
+                            </IconButton></td>}
+                        {role !== 'doctor' && <td><IconButton>
+                            <EditIcon onClick={(e: any) => {
+                                e.stopPropagation();
+                                dispatch(openManagerChangeModal(appl.id))
+                            }} /></IconButton>
+                        </td>}
+                    </tr>)}
                 </tbody>
             </table>
         </div>
-        {!isLoading && isEmpty(appls) && <div><BlockIcon sx={{fontSize: '40px', marginTop: '20px'}}/></div>}
-        {isLoading && <div><CircularProgress/></div>}
-        {!isLoading && error && <Typography sx={{color: 'red'}}>{errorMessage}</Typography>}
+        {!isLoading && isEmpty(appls) && <div><BlockIcon sx={{ fontSize: '40px', marginTop: '20px' }} /></div>}
+        {isLoading && <div><CircularProgress /></div>}
+        {!isLoading && error && <Typography sx={{ color: 'red' }}>{errorMessage}</Typography>}
         <BasicModal
             open={(deleteModalId as boolean)}
             onClose={() => setDeleteModal(false)}
@@ -144,12 +144,12 @@ const ReportList = (): React.ReactElement => {
                     <CommonButton
                         onClick={() => removeAppl(deleteModalId as number)}
                         color='error'
-                        title='Да'/>
+                        title='Да' />
                     <CommonButton
                         title='Нет'
-                        onClick={() => setDeleteModal(false)}/>
+                        onClick={() => setDeleteModal(false)} />
                 </div>
-            </Box>}/>
+            </Box>} />
         {/*</BasicModal>*/}
         {appls.count > 10 && <div className="pagination">
             <Pagination
