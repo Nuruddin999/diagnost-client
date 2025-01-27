391222968101433278ccc2ae57278cf31ea62383
nuruddin <sg7720@gmail.com>
2025-01-27 19:02:06 +0300

    рефактор

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,28 +1,20 @@
-export  const handleKeyDown = (prevField: any) => {
+const formatPhoneText=(nums:Array<string>)=>{
+    let rawText = "+ _ (___) ___ - __ - __"
+    for (let i = 0; i < nums.length; i++) {
+        rawText = i > 10 ? (rawText += ' - _').replace("_", nums[i]) : rawText.replace("_", nums[i]);
+    }
+    return rawText;
+}
+
+export const handleKeyDown = (prevField: any) => {
 
     const nums = prevField.match(/\d/g) || [];
     nums.pop(); // Удаляем последнюю цифру
-
-    // Переформатируем маску
-    let rawText = "+ _ (___) ___ - __ - __";
-    for (let i = 0; i < nums.length; i++) {
-        rawText = rawText.replace("_", nums[i]);
-    }
-
-    return rawText;
+    return formatPhoneText(nums);
 
 };
 
-export const formatPhone = (phone:string)=> {
-    const nums = phone.match(/\d/gi);
-
-    let rawText = "+ _ (___) ___ - __ - __"
-    if (nums) {
-        for (let i = 0; i < nums?.length; i++) {
-            rawText = rawText.replace("_", nums[i]);
-        }
-
-    }
-
-    return rawText
+export const formatPhone = (phone: string) => {
+    const nums = phone.match(/\d/gi) || [];
+    return formatPhoneText(nums);
 }
