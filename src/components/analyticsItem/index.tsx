9f33cada5d1afd6661cbd44ab1131d41e1c84ce4
nuruddin <sg7720@gmail.com>
2025-06-27 18:14:09 +0300

    correct user application duration

--- src/components/analyticsItem/index.tsx
+++ src/components/analyticsItem/index.tsx
@@ -19,6 +19,9 @@ const AnalyticsItem: FC = () => {
     const fromPeriod = new Date(data?.period[0] || '').toLocaleDateString()
     const toPeriod = new Date(data?.period[1] || '').toLocaleDateString()
 
+    const unfinished = data?.user.applications.filter(el=>el.passToCoordinatorTime === null)
+    const finished = data?.user.applications.filter(el=>el.passToCoordinatorTime !== null)
+
 
     useEffect(() => {
     }, []);
@@ -60,36 +63,32 @@ const AnalyticsItem: FC = () => {
                 <TableHead>
                     <TableRow>
                       <TableCell><Typography fontWeight={'bold'}>В работе</Typography></TableCell>
-                        <TableCell><Typography fontWeight={'bold'}>Время выполнения заявки</Typography></TableCell>
+                        <TableCell><Typography fontWeight={'bold'}>Завершенные</Typography></TableCell>
                         <TableCell><Typography fontWeight={'bold'}>Период</Typography></TableCell>
+                        <TableCell><Typography fontWeight={'bold'}>Время выполнения заявки</Typography></TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {data?.user.applications.filter(el=>el.passToCoordinatorTime !== null).map((el, index) => (
+                    {data?.user.applications.map((el, index) => (
                         <TableRow key={index}>
                             <TableCell>
                                 <Typography>
-                                    {el.name}
+                                    {unfinished && unfinished[index] ? unfinished[index].name:""}
                                 </Typography>
                             </TableCell>
                             <TableCell>
                                 <Typography>
-                                    {makeDuration(el.duration)}
+                                    {finished && finished[index] ? finished[index].name:""}
                                 </Typography>
                             </TableCell>
                             <TableCell>
                                 <Typography>
-
+                                    {`C ${fromPeriod} по ${toPeriod}`}
                                 </Typography>
                             </TableCell>
                             <TableCell>
                                 <Typography>
-
-                                </Typography>
-                            </TableCell>
-                            <TableCell>
-                                <Typography>
-
+                                    {finished && finished[index] ? makeDuration(finished[index].duration):""}
                                 </Typography>
                             </TableCell>
                         </TableRow>
